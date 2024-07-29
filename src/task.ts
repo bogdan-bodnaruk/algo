187fc78454887e8c02d1fff242b698cefc60c950
Bogdan Bodnaruk <bbodnar@softserveinc.com>
2024-07-29 12:11:54 +0300

    Update task.ts

--- src/task.ts
+++ src/task.ts
@@ -2,10 +2,6 @@
 Given a 2D grid of size m x n filled with positive numbers, find the path from the top-left corner
 to the bottom-right corner that minimizes the sum of all numbers along its path. You can only move
 either down or right at any point in time.
-
-Recommendation:
-Use recursion to solve this problem. The function minPathSumRec will be used to recursively
-calculate the minimum path sum to reach the bottom-right corner starting from the top-left corner.
  */
 
 export function minPathSum(grid: Grid) {
